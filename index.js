1e7f8687fa9130a90299235a14563dc434f9a277
wizardywizard <littledavid175@gmail.com>
2020-02-10 11:51:48 -0800

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function fetchBooks() {
-
+ return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    renderBooks(json)
+  });
 }
 
 function renderBooks(json) {
